6a6609aeb211c95c5468125d2eeea7ad70fb247b
Tripop Torcheep <tripop.tor@qualint.io>
2022-03-17 00:38:46 +0700

    update v4

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 import { Amplify } from "aws-amplify";
 import "@aws-amplify/ui-react/styles.css";
 import { AmplifyProvider } from "@aws-amplify/ui-react";
-import awsconfig from "./awsexports";
+import awsconfig from "./aws-exports";
 Amplify.configure(awsconfig);
 
 ReactDOM.render(
