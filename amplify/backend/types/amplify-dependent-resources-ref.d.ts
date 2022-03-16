2446ab8b68f1b2baf3ed1c27ad6a0a74570d13c2
Tripop Torcheep <tripop.tor@qualint.io>
2022-03-16 09:34:13 +0700

    Add storage, function, authenticate

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,5 +5,22 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "auth": {
+        "liontest": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s3e2ed816f": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 import {
-  CardA,
   CardACollection,
   FeatureDetailCollection,
   Features2x2,
-  Features4x1,
   HeroLayout2,
-  HeroLayout3,
+  MarketingFooter,
   NavBar,
 } from "./ui-components";
 import FeatureDetail from "./ui-components/FeatureDetail.jsx";
@@ -17,13 +14,14 @@ function App() {
     <div className="App">
       <NavBar />
       <HeroLayout2 />
-      <div style={{marginTop: 10, marginBottom: 10, padding: 30, height:241, backgroundColor: '#cfcfcf'}}>
+      <div style={{marginTop: 30, marginBottom: 30, padding: 30, height:241, backgroundColor: '#cfcfcf'}}>
         <FeatureDetailCollection />
       </div>
       <div className="Card">
         <CardACollection />
       </div>
       <Features2x2 />
+      <MarketingFooter />
     </div>
   );
 }
